3406fd1c00ff0baaec65868c6b427ae42f75fe0f
Eric Moras <121898077+Evickerest@users.noreply.github.com>
2023-10-17 22:49:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -59,6 +59,13 @@
                             <input type="text" name="team-number" required>
                         </div>
                     </div>
+                    <div class="radio-buttons">
+                        <div>
+                            <input type="checkbox" name="auto-driving" value="Broke" required>
+                            <label>Robot Broke</label>
+                        </div>
+                    </div>
+                    <input type="submit" id="broke" value="Submit" class="submit-button broke-button" formnovalidate>
                </div>
 
                 <!-- auto content  -->
@@ -67,23 +74,23 @@
                     <div class="divider"></div>
                     <div class="radio-buttons">
                         <div>
+                            <input type="checkbox" name="auto-driving" value="Engaged" required>
                             <label>Engaged</label>
-                            <input type="radio" name="auto-driving" value="Engaged" required>
                         </div>
                         <div>
+                            <input type="checkbox" name="auto-driving" value="Docked" required>
                             <label>Docked</label>
-                            <input type="radio" name="auto-driving" value="Docked" required>
                         </div>
                         <div>
-                            <label>Parked</label>
-                            <input type="radio" name="auto-driving" value="Parked" required>
+                            <input type="checkbox" name="auto-driving" value="Mobility" required>
+                            <label>Mobility</label>
                         </div>
                         <div>
+                            <input type="checkbox" data-id="none" name="auto-driving" value="None" required >
                             <label>None</label>
-                            <input type="radio" name="auto-driving" value="None" required >
                         </div>
                     </div>
-                    <div class="buttons">
+                    <!-- <div class="buttons">
                         <div class="button-info">
                             <span> Score:</span>
                             <span data-target="auto-score-view" value="1" class="button">+</span>
@@ -98,6 +105,58 @@
                             <input type="hidden" id="auto-misses" name="auto-misses" value="0">
                             <span data-target="auto-misses-view" value="-1" class="button">-</span>
                         </div>
+                    </div> -->
+                    <div class="teleop-score-container">
+                        <div style="color:orange" class="piece-logo centered">⧍</div>
+                        <div class="hits-title">Hits</div>
+                        <div class="misses-title">Misses</div>
+                        <div class="row-title centered">High</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="row-title centered">Mid</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div  class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="row-title centered">Low</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div  class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                    </div>
+                    <div class="teleop-score-container">
+                        <div class="piece-logo centered">🟪</div>
+                        <div class="hits-title">Hits</div>
+                        <div class="misses-title">Misses</div>
+                        <div class="row-title centered">High</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="row-title centered">Mid</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div  class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="row-title centered">Low</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div  class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
                     </div>
                 </div>
 
@@ -105,7 +164,7 @@
                 <div class="teleop-content tab-content">
                     <header>Teleop</header>
                     <div class="divider"></div>
-                    <div class="buttons">
+                    <!-- <div class="buttons">
                         <div class="button-info">
                             <span> Score:</span>
                             <span data-target="teleop-score-view" value="1" class="button">+</span>
@@ -120,6 +179,32 @@
                             <input type="hidden" id="teleop-misses" name="teleop-misses" value="0">
                             <span data-target="teleop-misses-view" value="-1" class="button">-</span>
                         </div>
+                    </div> -->
+                    <div class="teleop-score-container">
+                        <div style="color:orange" class="piece-logo centered">⧍</div>
+                        <div class="hits-title">Hits</div>
+                        <div class="misses-title">Misses</div>
+                        <div class="row-title centered">High</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="row-title centered">Mid</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div  class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="row-title centered">Low</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
+                        <div class="score-button centered" data-value="1">+</div>
+                        <div  class="score-value centered">0</div>
+                        <div class="score-button centered" data-value="-1">-</div>
                     </div>
                 </div>
 
@@ -130,26 +215,94 @@
                     <div class="divider"></div>
                     <div class="radio-buttons">
                         <div>
+                            <input type="checkbox" name="teleop-driving" value="Engaged" required>
                             <label>Engaged</label>
-                            <input type="radio" name="teleop-driving" value="Engaged" required>
                         </div>
                         <div>
+                            <input type="checkbox" name="teleop-driving" value="Docked" required>
                             <label>Docked</label>
-                            <input type="radio" name="teleop-driving" value="Docked" required>
                         </div>
                         <div>
+                            <input type="checkbox" name="teleop-driving" value="Parked" required>
                             <label>Parked</label>
-                            <input type="radio" name="teleop-driving" value="Parked" required>
                         </div>
                         <div>
+                            <input type="checkbox" data-id="none" name="teleop-driving" value="None" required>
                             <label>None</label>
-                            <input type="radio" name="teleop-driving" value="None" required>
+                        </div>
+                    </div>
+                    <div class="radio-buttons">
+                        <div>
+                            <input type="radio" name="teleop-qual" value="Attempted" required>
+                            <label>Attempted Double/Triple</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="teleop-qual" value="Double" required>
+                            <label>Double Climb</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="teleop-qual" value="Triple" required>
+                            <label>Triple Climb</label>
+                        </div>
+            
+                    </div>
+                    <div class="input-info">
+                        <div class="qual-container">
+                            <div class="qual">
+                                <div class="qual-label">Cycle Speed</div>
+                                <div class="divider quad-divider"></div>
+                                <div class="qual-stars-container">
+                                    <div data-value="1" class="star">★</div>
+                                    <div data-value="2" class="star">★</div>
+                                    <div data-value="3" class="star">★</div>
+                                    <div data-value="4" class="star">★</div>
+                                    <div data-value="5" class="star">★</div>
+                                    <span class="star-clear-button">CLEAR</span>
+                                </div>
+                            </div>
+                            <div class="qual">
+                                <div class="qual-label">Maneuverability</div>
+                                <div class="divider quad-divider"></div>
+                                <div class="qual-stars-container">
+                                    <div data-value="1" class="star">★</div>
+                                    <div data-value="2" class="star">★</div>
+                                    <div data-value="3" class="star">★</div>
+                                    <div data-value="4" class="star">★</div>
+                                    <div data-value="5" class="star">★</div>
+                                    <span class="star-clear-button">CLEAR</span>
+                                </div>
+                            </div>
+                            <div class="qual">
+                                <div class="qual-label">Defensive Ability</div>
+                                <div class="divider quad-divider"></div>
+                                <div class="qual-stars-container">
+                                    <div data-value="1" class="star">★</div>
+                                    <div data-value="2" class="star">★</div>
+                                    <div data-value="3" class="star">★</div>
+                                    <div data-value="4" class="star">★</div>
+                                    <div data-value="5" class="star">★</div>
+                                    <span class="star-clear-button">CLEAR</span>
+                                </div>
+                            </div>
+                            <div class="qual">
+                                <div class="qual-label">Overall Rating</div>
+                                <div class="divider quad-divider"></div>
+                                <div class="qual-stars-container">
+                                    <div data-value="1" class="star">★</div>
+                                    <div data-value="2" class="star">★</div>
+                                    <div data-value="3" class="star">★</div>
+                                    <div data-value="4" class="star">★</div>
+                                    <div data-value="5" class="star">★</div>
+                                    <span class="star-clear-button">CLEAR</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="input-info">
+                        <h2 id="comment-label">Comments</h2>
                         <div class="row">
-                            <label>Comments:</label>
-                            <input id="text-comment" type="text" name="comments">
+                            <!--<input id="text-comment" type="text" name="comments">-->
+                            <textarea id="text-comment" name="comments" rows="4"></textarea>
                         </div>
                     </div>
                     <input type="submit" class="submit-button">
