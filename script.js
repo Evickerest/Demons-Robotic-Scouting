88061230ab2ca326b687bfade8ccd22fecc9ddd3
Eric Moras <121898077+Evickerest@users.noreply.github.com>
2023-10-20 16:13:25 -0400

    tidying up

--- script.js
+++ script.js
@@ -18,42 +18,59 @@ buttons.forEach( button => {
 const tabs = document.querySelectorAll(".tab");
 const tabContents = document.querySelectorAll(".tab-content");
 
+
+// add click listener to each tab
 tabs.forEach( tab => {
-  
   tab.addEventListener( "click", () => {
+    // remove 'active' class from all tabs
     tabs.forEach(tab => tab.classList.remove("active"));
+
+    // remove 'tab-active' class from all tab content
     tabContents.forEach(content => content.classList.remove("tab-active"));
 
+    //set tab content to active
     document.querySelector(tab.dataset.target).classList.add("tab-active");
     tab.classList.add("active");
   });
 });
 
-qualStars = document.querySelectorAll(".star");
+const qualStars = document.querySelectorAll(".star");
 
+// add click listener to each star
 qualStars.forEach( star => {
     star.addEventListener("click", ()=> {
         recheckStars( star );
     });
 });
 
-starClearButtons = document.querySelectorAll(".star-clear-button");
+const starClearButtons = document.querySelectorAll(".star-clear-button");
 
+// listener for 'clear' button
 starClearButtons.forEach( button => {
     button.addEventListener("click", ()=>{
         recheckStars( button );
     });
 });
 
+//
 function recheckStars( buttonPressed ){
-    siblingStars = buttonPressed.parentElement.querySelectorAll(".star");
+    //gets sibling stars based on parentElement of button
+    const siblingStars = buttonPressed.parentElement.querySelectorAll(".star");
+
+    //gets input element baed on parentElement of button
+    const starInputField = buttonPressed.parentElement.querySelector("input");
 
     if( buttonPressed.textContent == "clear"){
         valueFilter = 0;
     } else {
         valueFilter = buttonPressed.dataset.value;
     }
+
+    // for some reason.
+    // if || 0 is not there value will somehow end up undefined
+    starInputField.value = valueFilter || 0;
     
+    // select stars and deselect stars based on filter
     siblingStars.forEach( star => {
         if( star.dataset.value <= valueFilter){
             star.classList.add("star-checked");
@@ -63,40 +80,44 @@ function recheckStars( buttonPressed ){
     }); 
 }
 
-noneBoxes = document.querySelectorAll("input[data-id='none']");
 
-noneBoxes.forEach(noneBox => {
-    noneBox.addEventListener("change", () => {
-        siblingBoxes = noneBox.parentElement.parentElement.querySelectorAll("input:not([data-id='none'])");
-        siblingBoxes.forEach(box => {
-            box.checked = false;
-        })
-    });
-});
+const robotBrokeBox = document.querySelector("input[value='Broke']");
+const brokeSubmitButton = document.querySelector("#broke-button");
+const submitButton = document.querySelector("#aaaa");
 
-robotBrokeBox = document.querySelector("input[value='Broke']");
-brokeSubmitButton = document.querySelector("#broke");
 
+// one submit button requires some fields to be filled out, when one submit button does not
+// this code toggles visibility between them based on if the robot broke or not
 robotBrokeBox.addEventListener("change", () => {
-    vis = robotBrokeBox.checked ? "block" : "none";
-    brokeSubmitButton.style.display = vis;
+    const brokeBool = robotBrokeBox.checked;
+    submitButton.style.display = brokeBool ? "none" : "block";
+    brokeSubmitButton.style.display = brokeBool ? "block" : "none";
 });
 
-scoreButtons = document.querySelectorAll(".score-button");
+const scoreButtons = document.querySelectorAll(".score-button");
 
+// the goal of this code is that it finds the nearest score and changes it
 scoreButtons.forEach(button => {
     button.addEventListener("click", () => {
-        var previousSibling = button.previousElementSibling;
+
+        // fancy way of finding nearest number score
+        var previousSibling = button.previousElementSibling.previousElementSibling;
         var score;
         if( [...previousSibling.classList].includes("score-value")){
             score = previousSibling;
         } else {
             score = button.nextElementSibling;
         }
+
+        //change score by converting string -> integer
         const buttonValue = button.dataset.value;
         const currentScore = score.textContent;
-        const newScore = parseInt(currentScore) + parseInt(buttonValue);
-        score.textContent = Math.max(0,newScore);
+        const newScore = Math.max(0, parseInt(currentScore) + parseInt(buttonValue));
+        score.textContent = newScore;
+
+        //change input element value
+        const scoreInputField = score.nextElementSibling;
+        scoreInputField.value = newScore;
     });
 });
 
